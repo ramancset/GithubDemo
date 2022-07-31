0be5d0e85bcdd484c82f7c0749a14f1a7a9bcbe2
Raman Jha <Raman Jha@DESKTOP-17PQN8B>
2022-07-31 14:51:38 +0530

    fist commit

---
+++ src/test/java/RamanDemo/MasterClass.java
@@ -0,0 +1,12 @@
+package RamanDemo;
+
+public class MasterClass {
+
+	public static void main(String[] args)
+	{
+		System.out.println("main method");
+		
+
+	}
+
+}
---
+++ src/test/java/RamanDemo/MasterClass2.java
@@ -0,0 +1,12 @@
+package RamanDemo;
+
+public class MasterClass2 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("main method2");
+		
+
+	}
+
+}
